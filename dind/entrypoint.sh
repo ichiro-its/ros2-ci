098c2cc6f61839c3a706cb87a452bc6461358b8e
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-06 14:17:24 +0700

    Fix log output view

--- dind/entrypoint.sh
+++ dind/entrypoint.sh
@@ -6,18 +6,28 @@ POST_BUILD="$3"
 PRE_TEST="$4"
 POST_TEST="$5"
 
-echo '\n======== Running the Docker daemon ========\n'
+echo ''
+echo '======== Running the Docker daemon ========'
+echo ''
+
 dockerd-entrypoint.sh &
 sleep 10
 
 mkdir "/ros2/ws" && cp -r "${GITHUB_WORKSPACE}" "/ros2/ws" || exit $?
 
-echo '\n======== Building the ROS 2 image ========\n'
+echo ''
+echo '======== Building the ROS 2 image ========'
+echo ''
+
+
 docker build \
   --build-arg ROS2_DISTRO="${ROS2_DISTRO}" \
   -t ros2-ci:latest /ros2 || exit $?
 
-echo '\n======== Running the ROS 2 container ========\n'
+echo ''
+echo '======== Running the ROS 2 container ========'
+echo ''
+
 docker run \
   --env PRE_BUILD="${PRE_BUILD}" \
   --env POST_BUILD="${POST_BUILD}" \
