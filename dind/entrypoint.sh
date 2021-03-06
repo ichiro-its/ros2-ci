c36eddb5866923ff9d3359d7289a80cf4a472010
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-03-06 14:04:54 +0700

    Better debug log

--- dind/entrypoint.sh
+++ dind/entrypoint.sh
@@ -6,18 +6,18 @@ POST_BUILD="$3"
 PRE_TEST="$4"
 POST_TEST="$5"
 
-echo '======== Running the Docker daemon ========'
+echo '\n======== Running the Docker daemon ========\n'
 dockerd-entrypoint.sh &
 sleep 10
 
 mkdir "/ros2/ws" && cp -r "${GITHUB_WORKSPACE}" "/ros2/ws" || exit $?
 
-echo '======== Building the ROS 2 image ========'
+echo '\n======== Building the ROS 2 image ========\n'
 docker build \
   --build-arg ROS2_DISTRO="${ROS2_DISTRO}" \
   -t ros2-ci:latest /ros2 || exit $?
 
-echo '======== Running the ROS 2 container ========'
+echo '\n======== Running the ROS 2 container ========\n'
 docker run \
   --env PRE_BUILD="${PRE_BUILD}" \
   --env POST_BUILD="${POST_BUILD}" \
--- dind/ros2/entrypoint.bash
+++ dind/ros2/entrypoint.bash
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-echo '======== Loading the ROS 2 environment ========'
-CMD='source /opt/ros/${ROS2_DISTRO}/setup.sh'
+echo '\n======== Loading the ROS 2 environment ========\n'
+CMD="source /opt/ros/${ROS2_DISTRO}/setup.sh"
 echo "$CMD" && eval "$CMD" || exit $?
 
 if [ ! -z "$PRE_BUILD" ]; then
-  echo '======== Running the pre-build command ========'
+  echo '\n======== Running the pre-build command ========\n'
   echo "$PRE_BUILD" && eval "$PRE_BUILD" || exit $?
 fi
 
-echo '======== Building the workspace ========'
+echo '\n======== Building the workspace ========\n'
 cd /ws && colcon build \
   --event-handlers console_cohesion+ \
   --cmake-args \
   --symlink-install || exit $?
 
 if [ ! -z "$POST_BUILD" ]; then
-  echo '======== Running the post-build command ========'
+  echo '\n======== Running the post-build command ========\n'
   echo "$POST_BUILD" && eval "$POST_BUILD" || exit $?
 fi
 
 if [ ! -z "$PRE_TEST" ]; then
-  echo '======== Running the pre-test command ========'
+  echo '\n======== Running the pre-test command ========\n'
   echo "$PRE_TEST" && eval "$PRE_TEST" || exit $?
 fi
 
-echo '======== Testing the workspace ========'
+echo '\n======== Testing the workspace ========\n'
 cd /ws && colcon test \
   --event-handlers console_cohesion+ \
   --pytest-with-coverage \
   --return-code-on-test-failure || exit $?
 
 if [ ! -z "$POST_TEST" ]; then
-  echo '======== Running the post-test command ========'
+  echo '\n======== Running the post-test command ========\n'
   echo "$POST_TEST" && eval "$POST_TEST" || exit $?
 fi
